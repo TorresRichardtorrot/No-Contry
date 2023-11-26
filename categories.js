2c634c9dc1b8d5967d63d138dcd5d2bd8d01adad
PancoBrovia <136863761+PancoBrovia@users.noreply.github.com>
2023-11-26 14:47:17 -0300

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,9 @@
+export class user {
+    constructor(
+        id,
+        name
+    ) { 
+    this.id = id
+    this.name = name
+}
+}
---
+++ products.js
@@ -0,0 +1,17 @@
+export class user {
+constructor ( 
+id,
+name,
+price,
+description,
+quantity,
+id_category,
+){
+this.id = id
+this.name = name
+this.price = price
+this.description = description
+this.quantity = quantity
+this.id_category = id_category
+}
+}
